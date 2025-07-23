3c19dfe73e533d6fe7d6c7c82e11e6df35a44f60
Navid Talebian <navidt81@gmail.com>
2025-07-23 17:55:28 +0330

    feat: improve tree component set dynamic pages for files

--- packages/fanoos/app/components/tree.vue
+++ packages/fanoos/app/components/tree.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { TreeItem, TreeRoot } from 'reka-ui';
 
+const route = useRoute();
+
 const { data } = useFetch<TreeNode[]>('/api/scanner/tree', {
   lazy: true,
   server: false,
@@ -8,47 +10,49 @@ const { data } = useFetch<TreeNode[]>('/api/scanner/tree', {
 </script>
 
 <template>
-  <div class="px-10 py-3">
-    <TreeRoot
-      v-slot="{ flattenItems }"
-      class="list-none select-none w-72 bg-dar text-gray-50 rounded-lg shadow-sm p-2 text-sm font-medium"
-      :items="data"
-      :get-key="(item) => item.path"
-    >
-      <h2 class="font-bold text-sm text-gray-50 px-2 pt-1 pb-3">
-        Directory Structure
-      </h2>
-      <TreeItem
-        v-for="item in flattenItems"
-        v-slot="{ isExpanded }"
-        :key="item._id"
-        :style="{ 'padding-left': `${item.level - 0.5}rem` }"
-        v-bind="item.bind"
-        class="flex justify-between items-center my-0.5 py-1 px-2 rounded outline-none "
+  <div class="py-3">
+    <UiScrollArea>
+      <TreeRoot
+        v-slot="{ flattenItems }"
+        class="list-none select-none w-72 max-h-full no-scrollbar overflow-y-scroll bg-dar rounded-lg shadow-sm p-2 text-sm font-medium"
+        :items="data"
+        :get-key="(item) => item.path"
       >
-        <div class="flex items-center w-full grow">
-          <Icon
-            v-if="item.hasChildren"
-            name="material-symbols:keyboard-arrow-up"
-            class="w-max text-xl"
-            :class="isExpanded ? 'rotate-180' : 'rotate-90'"
-          />
-          <div v-else class="w-5" />
-          <Icon
-            v-if="item.hasChildren"
-            :name="isExpanded ? 'folder-open' : 'folder'"
-            class="text-primary text-xl"
-          />
-          <Icon
-            v-else
-            class="text-xl"
-            :name="item.value.ext.slice(1) || 'lucide:file'"
-          />
-          <div class="pl-2 shrink truncate max-w-32">
-            {{ item.value.name }}
+        <h2 class="font-bold text-sm  px-2 pt-1 pb-3">
+          Directory Structure
+        </h2>
+        <TreeItem
+          v-for="item in flattenItems"
+          v-slot="{ isExpanded }"
+          :key="item._id"
+          v-bind="item.bind"
+          class="flex justify-between items-center px-2 outline-none "
+        >
+          <div class="flex">
+            <div v-for="n in item.level" :key="n" class="first:invisible first:ml-0 border-l border-white/20 h-7 mr-3 ml-2 last:mr-1" />
           </div>
-        </div>
-      </TreeItem>
-    </TreeRoot>
+          <div class="w-full cursor-pointer">
+            <div v-if="item.hasChildren" class="flex items-center">
+              <Icon
+                :name="isExpanded ? 'folder-open' : 'folder'"
+                class="text-primary text-xl mx-2"
+              />
+              <div class="shrink truncate max-w-32">
+                {{ item.value.name }}
+              </div>
+            </div>
+            <NuxtLink v-else :to="`/tree/${item.value.path}`" class="flex hover:opacity-80 items-center duration-300 py-1 px-2 rounded-md" :class="{ 'bg-primary/50': route.path === `/tree/${item.value.path}` }">
+              <Icon
+                class="text-xl  "
+                :name="item.value.ext.slice(1) || 'lucide:file'"
+              />
+              <div class="pl-2 shrink truncate max-w-32">
+                {{ item.value.name }}
+              </div>
+            </NuxtLink>
+          </div>
+        </TreeItem>
+      </TreeRoot>
+    </UiScrollArea>
   </div>
 </template>
---
+++ packages/fanoos/app/pages/tree/[...dir].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div class="text-2xl text-white">
+    {{ route.path }}
+  </div>
+</template>
