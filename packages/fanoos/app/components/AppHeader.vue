1e49a6157ef0265c1557996830fe8aa2fe60ff83
Navid Talebian <navidt81@gmail.com>
2025-07-30 11:32:53 +0330

    refactor: change components from reka-ui to nuxt ui

---
+++ packages/fanoos/app/components/AppHeader.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const items = computed(() => [
+  {
+    label: 'Docs',
+    to: '/getting-started',
+    active: route.path.startsWith('/getting-started'),
+  },
+  {
+    label: 'Issues',
+    to: 'https://github.com/fanoosjs/fanoos/isssus',
+    target: '_blank',
+  },
+  {
+    label: 'Releases',
+    to: 'https://github.com/fanoosjs/fanoos/releases',
+    target: '_blank',
+  },
+]);
+</script>
+
+<template>
+  <UHeader>
+    <template #title>
+      <div class="flex items-center gap-2">
+        <img src="../assets/images/FanoosJS-logo.png" alt="Fanoos Logo" class="w-12">
+        <div class="text-2xl">
+          Fanoos
+        </div>
+      </div>
+    </template>
+
+    <UNavigationMenu :items />
+
+    <template #right>
+      <UColorModeButton />
+
+      <UButton
+        color="neutral"
+        variant="ghost"
+        to="https://github.com/fanoosjs/fanoos"
+        target="_blank"
+        icon="i-simple-icons-github"
+        aria-label="GitHub"
+      />
+    </template>
+  </UHeader>
+</template>
---
+++ packages/fanoos/app/components/AppTree.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { TreeItem } from '#ui/types';
+
+const { data } = useFetch<TreeItem[]>('/api/scanner/tree', {
+  lazy: true,
+  server: false,
+});
+
+const treeItem = ref();
+</script>
+
+<template>
+  {{ treeItem?.path }}
+  <UTree
+
+    v-model="treeItem"
+    :items="data"
+  />
+</template>
