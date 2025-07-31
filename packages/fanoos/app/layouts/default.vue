d77c5f5268443cfd49486488883849b94ba19e2d
Navid Talebian <navidt81@gmail.com>
2025-07-31 10:51:50 +0330

    implement layout of dashboard

--- packages/fanoos/app/layouts/default.vue
+++ packages/fanoos/app/layouts/default.vue
@@ -1,13 +1,5 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="bg-dark h-screen overflow-y-scroll text-gray-50 w-full">
-    <div class="max-w-[1440px] mx-auto h-full">
-      <Header />
-      <div class="flex gap-10 h-full">
-        <Tree />
-        <slot />
-      </div>
-    </div>
+  <div class="">
+    <slot />
   </div>
 </template>
