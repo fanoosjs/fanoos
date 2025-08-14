b9ec05b48c6fd1137c548547fd947d1f6b5e2acf
Navid Talebian <navidt81@gmail.com>
2025-08-14 09:58:44 +0330

    init chart in vue component

---
+++ packages/fanoos/app/components/LOC/item.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  color: string;
+  size: number;
+  percentage: number;
+}>();
+</script>
+
+<template>
+  <div :style="{ backgroundColor: color, width: `${percentage}%` }" class="flex items-center justify-center">
+    {{ size }}
+  </div>
+</template>
